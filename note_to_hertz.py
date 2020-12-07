d597c6d4637908d95faefecef81280bef5409eb6
Vinanddrinks <66210430+Vinanddrinks@users.noreply.github.com>
2020-12-07 11:40:32 +0100

    Create note_to_hertz.py
    added function note to hertz which convert a string note to it's hertzian base frequency
    
    Co-Authored-By: RiccardiG <75377320+RiccardiG@users.noreply.github.com>

---
+++ note_to_hertz.py
@@ -0,0 +1,11 @@
+def note_to_hertz(note):
+    dic ={
+        "DO" : 264,
+        "RE" : 297,
+        "MI" : 330,
+        "FA" : 352,
+        "SOL": 396,
+        "LA" : 440,
+        "Si" : 495,
+    }
+    return dic[note]
