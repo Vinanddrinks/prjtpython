bada378e3ca5320db8b6f152252cbf8c51ad6b99
RiccardiG <75377320+RiccardiG@users.noreply.github.com>
2020-12-08 11:57:05 +0100

    Update readfile.py
    Updated Readfile()

--- readfile.py
+++ readfile.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python
+partitions = open("partitions.txt", "r")
+dic = {
+
+}
+
+for line_number, lines in enumerate(partitions):
+    if (line_number % 2) == 0:
+        dic[lines] = 1
+for key in dic:
+    for line_number, lines in enumerate(partitions):
+        if (line_number % 2) != 0:
+            dic[key] = lines
+
+print(dic)
+
+
