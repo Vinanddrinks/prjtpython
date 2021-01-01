6d37207a90adb388407e35a0f2c75caadc25b988
Vinanddrinks <66210430+Vinanddrinks@users.noreply.github.com>
2021-01-01 16:22:53 +0100

    Update Main.py
    add importation and initialization step

--- Main.py
+++ Main.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python
+#importation block
 from read.functionshapetotime import *
 from read.notetohertz import *
 from read.partitiontolist import *
 from read.playpartition import *
 from files.readfile import *
 from analytics.new_melody import *
+import tkinter as tk
+from pathlib import Path
+# end of importation block
+# database initialization 
+if Path('pdata.json').is_file():
+    partidic = readfilejson()
+else:
+    partidic = readfiletxt()
+# end database initialization
+
