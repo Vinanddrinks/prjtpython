f767c84265b7843c14b23ffbe82e58cf9ebffa97
Vinanddrinks <66210430+Vinanddrinks@users.noreply.github.com>
2021-01-01 15:54:55 +0100

    Update Main.py
    add imports

--- Main.py
+++ Main.py
@@ -3,3 +3,5 @@ from read.functionshapetotime import *
 from read.notetohertz import *
 from read.partitiontolist import *
 from read.playpartition import *
+from files.readfile import *
+from analytics.new_melody import *
