6ce9ea66b4f1795c833fdb8b7a065e92aa460db0
Vinanddrinks <66210430+Vinanddrinks@users.noreply.github.com>
2021-01-02 17:58:39 +0100

    update main.py
    adding subfontion for command button on tkinter

--- Main.py
+++ Main.py
@@ -15,8 +15,28 @@ if Path('pdata.json').is_file():
 else:
     partidic = readfiletxt()
 # end database initialization
-# tkinter commands functions
-# end tkinter commands functions
+# tkinter commands button functions
+def new_sheet():
+    global partidic
+    w = tk.Tk()
+    w.title("Adding New sheet")
+
+    cadre = tk.Frame(w, bg= "#7EAFE0")
+    cadre.pack(expand=tk.YES)
+
+    cancel = tk.Button(cadre,text="Cancel",command= lambda : w.destroy())
+    cancel.pack()
+    w.mainloop()
+def Markov():
+    global partidic
+    w = tk.Tk()
+    w.title("Markov derivation")
+    w.mainloop()
+def kill_save():
+    global partidic,root
+    writejson(partidic)
+    root.destroy()
+# end tkinter commands button functions
 # tkinter display
 # main window settings
 root = tk.Tk()
@@ -27,13 +47,32 @@ root.maxsize(1920, 1080)
 root.iconbitmap("root.ico")
 root.config(bg="#7EAFE0")
 # end main widow settings
+# frame configuration
+cadre = tk.Frame(root, bg= "#7EAFE0")
+cadre.pack(expand=tk.YES)
 # description title
-desc = tk.Label(root, text="Welcome to Music and Statistic, a program to derive melody from other's", font=(
+desc = tk.Label(cadre, text="Welcome to Music and Statistic, a program to derive melody from other's", font=(
     "Helvetica", 20), bg="#7EAFE0")
 desc.pack(expand=tk.YES)
 # end description title
 
+# new sheet button
+kills = tk.Button(cadre,text=" Add new sheet ",font=("Helvetica", 18), bg = "#FFFFFF" ,fg = "#000000", command = lambda : new_sheet())
+kills.pack(expand=tk.YES)
+#end new sheet button
+
+# new sheet button
+killt = tk.Button(cadre,text=" tarkov derivation ",font=("Helvetica", 18), bg = "#FFFFFF" ,fg = "#000000", command = lambda : Markov())
+killt.pack(expand=tk.YES)
+#end new sheet button
 # kill button
+killSb = tk.Button(cadre,text=" Exit & save program ",font=("Helvetica", 18), bg = "#FFFFFF" ,fg = "#000000", command = lambda : kill_save())
+killSb.pack(expand=tk.YES)
+#end kill button
+# kill button
+killb = tk.Button(cadre,text=" Exit program ",font=("Helvetica", 18), bg = "#FFFFFF" ,fg = "#000000", command = lambda : root.destroy())
+killb.pack(expand=tk.YES)
+#end kill button
 
 root.mainloop()  # indicateur de boucle sur la fenêtre principale
 #end tkinter display
