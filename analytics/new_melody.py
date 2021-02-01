725656146c0376a533f3d7da28196297cf50f4be
RiccardiG <75377320+RiccardiG@users.noreply.github.com>
2021-02-01 15:57:59 +0100

    Update new_melody.py
    returned to normal state

--- analytics/new_melody.py
+++ analytics/new_melody.py
@@ -102,6 +102,3 @@ def main_markov(partition):
     string_song = string_song[:-1]
     # We remove the last space so the string ends with a note and not a blank space " ".
     return string_song
-
-
-test
